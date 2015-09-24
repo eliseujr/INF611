f872aae4edee3c34c016f5e9fff30034aafb6bf6
Eliseu Massing <eliseujr@gmail.com>
2015-09-24 18:15:28 -0300

    Comecando a criar a estrutura de dados

--- trabalho.R
+++ trabalho.R
@@ -12,8 +12,19 @@ connection <- url("http://www.ic.unicamp.br/~zanoni/cepagri/cepagri.csv")
 cpa_data <- read.csv(connection, header = FALSE, sep = ";",
                      col.names = c("horario","temperatura","vento","umidade","sensacao"))
 
-# Parse time to the format YYYY-MM-DD HH:MM:SS
-cpa_data[[1]] <- strptime(cpa_data[[1]], "%d/%m/%Y-%H:%M")
+# Parse time to the format YYYY-MM-DD
+cpa_data[[1]] <- strptime(cpa_data[[1]], "%d/%m/%Y")
 
-cpa_data <- data.frame(Horario=cpa_data[1], Temperatura=cpa_data[2])
-colnames(cpa_data) <- c("Horario", "Temperatura")
+cpa_data <- data.frame(Horario=cpa_data[1], as.numeric((Temperatura=cpa_data[2])))
+#colnames(cpa_data) <- c("Horario", "Temperatura")
+
+serie <- list()
+
+#FIXME aqui so estou pegando as primeiras 100 linhas pra testar
+for(i in 1:100) {
+  # pegando o primeiro dia
+  day <- as.character(cpa_data[i, ][[1]])
+  
+  # atribuindo o vetor/lista de temperaturas na lista serie[dia]
+  #serie[as.character(cpa_data[i, ][[1]])] <- c()
+}
