ee751bebe7cff15472e5fe39351130fa3c8ff841
Eliseu Massing <eliseujr@gmail.com>
2015-09-24 21:26:02 -0300

    Consertando o factors... Precisamos criar a estrutura de dados agora

--- trabalho.R
+++ trabalho.R
@@ -9,22 +9,24 @@
 connection <- url("http://www.ic.unicamp.br/~zanoni/cepagri/cepagri.csv")
 
 # Parse cepagri data to a dataframe
-cpa_data <- read.csv(connection, header = FALSE, sep = ";",
+cpa_data <- read.csv(connection, header = FALSE, sep = ";", stringsAsFactors = F,
                      col.names = c("horario","temperatura","vento","umidade","sensacao"))
 
 # Parse time to the format YYYY-MM-DD
 cpa_data[[1]] <- strptime(cpa_data[[1]], "%d/%m/%Y")
 
-cpa_data <- data.frame(Horario=cpa_data[1], as.numeric((Temperatura=cpa_data[2])))
+cpa_data <- data.frame(Horario=cpa_data[1], Temperatura=cpa_data[2])
 #colnames(cpa_data) <- c("Horario", "Temperatura")
 
-serie <- list()
+#TODO Tenho que pensar em como definir a estrutura de dados 
+serie <- list(day = "", temps = c())
 
 #FIXME aqui so estou pegando as primeiras 100 linhas pra testar
 for(i in 1:100) {
-  # pegando o primeiro dia
+
   day <- as.character(cpa_data[i, ][[1]])
+  temp <- as.numeric(cpa_data[i, ][[2]])
   
-  # atribuindo o vetor/lista de temperaturas na lista serie[dia]
-  #serie[as.character(cpa_data[i, ][[1]])] <- c()
+
+  #serie[day] <- c(???, temp)
 }
